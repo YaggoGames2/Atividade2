befaf4b1632db16a4413bedb95e08e546aa45d50
YAGGO ROSA DE ALMEIDA SILVA <rosayaggo2@gmail.com>
2025-05-12 17:30:18 -0300

    12/5

--- javascript.js
+++ javascript.js
@@ -1,21 +1,21 @@
 function main() {
+    let numero1, numero2, resultado, saidacontagem = "";
 
-    let numero1, numero2, resultado;
-    let impar, par;
+    numero1 = Number(prompt("Digite o  primeiro número"));
+    numero2 = Number(prompt("Digite o segundo número maior do que o primeiro"));
 
-    numero1 = prompt("Digite o  primeiro número");
-    numero2 = prompt("Digite o segundo número maior do que o primeiro")
-  
-    resultado = numero1
+    resultado = numero1 + 1;
 
     while (resultado < numero2) {
         if (resultado % 2 == 0) {
-       resultado=alert( resultado + " esse número será par ");
+            saidacontagem=saidacontagem+resultado+"- par\n" ;
         } else {
-            resultado=alert( resultado + " esse número será impar ");
+            saidacontagem=saidacontagem+resultado+"- ímpar\n";
         }
+        resultado = resultado + 1;
     }
-
+    console.log(saidacontagem);
+    alert(saidacontagem);
 }
 
 main()
